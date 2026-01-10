66d581ed442b0c21efb612fd88f4065a8129a6d5
Mohammed Bilal <bilalputhige@gmail.com>
2026-01-10 09:16:39 +0530

    Update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-     baseURL:"https://gasco-0x4w.onrender.com";
+     baseURL:"https://gasco-0x4w.onrender.com"
    // baseURL:"http://localhost:3090"
 })
